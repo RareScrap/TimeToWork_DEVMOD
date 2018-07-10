493a2d99f2ad8b13097f6c026bab5a92af43c12d
RareScrap <RareScrap@users.noreply.github.com>
2018-07-10 14:06:54 +0800

    Базовый функционал

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -2,19 +2,43 @@ package com.example.examplemod;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
+import net.minecraft.util.StatCollector;
+
+import java.awt.*;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void ready(FMLLoadCompleteEvent event) throws AWTException {
+        if (SystemTray.isSupported()) { // TODO: Отображать пуш, только если окно игры не в форграунде
+            displayTray();
+        } else {
+            System.err.println("System tray not supported!");
+        }
+    }
+
+    public void displayTray() throws AWTException {
+        //Obtain only one instance of the SystemTray object
+        SystemTray tray = SystemTray.getSystemTray();
+
+        //If the icon is a file
+        Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
+        //Alternative (if the icon is on the classpath):
+        //Image image = Toolkit.getToolkit().createImage(getClass().getResource("icon.png"));
+
+        TrayIcon trayIcon = new TrayIcon(image, StatCollector.translateToLocal("tray.title")); // TODO: Клик по пушу открывает окно игры
+        //Let the system resize the image if needed
+        trayIcon.setImageAutoSize(true);
+        //Set tooltip text for the tray icon
+        trayIcon.setToolTip("System tray icon demo");
+        tray.add(trayIcon);
+
+        trayIcon.displayMessage(StatCollector.translateToLocal("tray.title")
+                , StatCollector.translateToLocal("tray.tooltip"), TrayIcon.MessageType.INFO);
     }
 }
